cbb37ea6800ae1aed6d49af5453c909f9703ad95
Shannu187 <shannukambhampati@gmail.com>
2023-11-28 12:03:12 -0600

    episode-02

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div", 
-    {id: "parent"},[
-         
+    {id: "parent"},[ 
     React.createElement("div",  {id: "child1"}, [
-        React.createElement("h1",{},"I am an H1 Tag"), 
+        React.createElement("h1",{},"I am an Shanmukh Tag"), 
         React.createElement("h2", {}, "I am an H2 Tag")
     ]), 
-
-     
     React.createElement("div",  {id: "child2"}, [
         React.createElement("h1",{},"I am an H1 Tag"), 
         React.createElement("h2", {}, "I am an H2 Tag")
     ])
-    ]    
-);
+]);
 
 console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(parent); 
